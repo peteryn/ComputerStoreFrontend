655d9e9d79ce0dc86c6d7a712fd05b20c894f1ea
Peter Yuan <peter.yuan18@gmail.com>
2024-09-05 17:58:22 -0400

    Add profie page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,15 +2,20 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import { User } from '../user';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-profile',
 	standalone: true,
-	imports: [],
+	imports: [FormsModule, CommonModule],
 	templateUrl: './profile.component.html',
 	styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+	model = new User('', '', '', '');
+
 	constructor(private http: HttpClient, private router: Router) {
 		console.log('ran');
 		this.http
@@ -23,6 +28,22 @@ export class ProfileComponent {
 			)
 			.subscribe((res: any) => {
 				console.log(res);
+			});
+		
+		this.http
+			.get('/api/details')
+			.subscribe((res: any) => {
+				console.log(res);
+				this.model.firstName = res.firstName;
+				this.model.lastName = res.lastName;
+			});
+	}
+
+	onSubmit() {
+		this.http
+			.put('/api/update', {"firstName" : this.model.firstName, "lastName": this.model.lastName})
+			.subscribe((res: any) => {
+
 			});
 	}
 }
