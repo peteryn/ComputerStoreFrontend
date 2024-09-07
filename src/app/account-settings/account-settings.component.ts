6e0d94af881eed1abdf2a870c4e57b75daa06e7b
Peter Yuan <peter.yuan18@gmail.com>
2024-09-06 21:47:24 -0400

    Add unfinished change password feature

---
+++ src/app/account-settings/account-settings.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { catchError, throwError } from 'rxjs';
+import { User } from '../user';
+
+@Component({
+	selector: 'app-account-settings',
+	standalone: true,
+	imports: [FormsModule],
+	templateUrl: './account-settings.component.html',
+	styleUrl: './account-settings.component.css',
+})
+export class AccountSettingsComponent {
+	constructor(private http: HttpClient) {}
+
+	oldPassword: string = '';
+	newPassword: string = '';
+
+	onSubmit() {
+		const form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement;
+		if (form.checkValidity()) {
+			this.http
+				.put('/api/changePassword', {
+					oldPassword: this.oldPassword,
+					newPassword: this.newPassword,
+				})
+				.pipe(
+					catchError((error: HttpErrorResponse) => {
+						console.log('Invalid response');
+						return throwError(() => error);
+					})
+				)
+				.subscribe(() => {
+					console.log('Sucessfully updated');
+				});
+
+			console.log('passed');
+		} else {
+			form.classList.add('was-validated');
+		}
+	}
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TestComponent } from './test/test.component';
 import { authorizationGuard } from './authorization.guard';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'register', component: RegisterComponent },
 	{ path: 'login', component: LoginComponent },
+	{
+		path: 'account-settings',
+		component: AccountSettingsComponent,
+		canActivate: [authorizationGuard],
+	},
 	{ path: 'profile', component: ProfileComponent },
 	{ path: 'test', component: TestComponent, canActivate: [authorizationGuard] },
 ];
